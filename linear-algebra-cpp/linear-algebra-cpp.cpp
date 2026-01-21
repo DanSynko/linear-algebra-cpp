befdde86c16ff58ecec1b48898452ba263c2cd5e
Данило <@LAPTOP-7M1VSVI1>
2026-01-21 16:03:40 +0200

    Добавьте файлы проекта.

---
+++ linear-algebra-cpp/linear-algebra-cpp.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+struct Vector {
+    using component = float;
+
+    component x, y, z;
+
+    float get_length() {
+        
+    }
+
+    
+    const Vector operator+(const Vector& vec) const {
+        
+    }
+    const Vector operator-(const Vector& vec) const {
+        
+    }
+
+    float dot_product(const Vector& other_vec) const {
+        
+    }
+    void normalize() {
+        
+    }
+};
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+
+    return 0;
+}
