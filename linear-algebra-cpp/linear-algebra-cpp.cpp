f818b88b0fb31593c158ef6b4a712eaef6663d0c
Данило <@LAPTOP-7M1VSVI1>
2026-01-21 17:48:06 +0200

    feat: implement basic Vector3 operations
    - Add vector addition, subtraction, length, dot product and normalize.
    - Overload operator<< for debug output.

--- linear-algebra-cpp/linear-algebra-cpp.cpp
+++ linear-algebra-cpp/linear-algebra-cpp.cpp
@@ -1,34 +1,61 @@
 ﻿#include <iostream>
 
-struct Vector {
-    using component = float;
 
+struct Vector3 {
+    using component = float;
     component x, y, z;
 
-    float get_length() {
-        
+    float get_length() const {
+        return sqrt((x * x) + (y * y) + (z * z));
     }
 
-    
-    const Vector operator+(const Vector& vec) const {
-        
+
+    const Vector3 operator+(const Vector3& vec) const {
+        return Vector3(this->x + vec.x, this->y + vec.y, this->z + vec.z);
     }
-    const Vector operator-(const Vector& vec) const {
-        
+    const Vector3 operator-(const Vector3& vec) const {
+        return Vector3(this->x - vec.x, this->y - vec.y, this->z - vec.z);
     }
 
-    float dot_product(const Vector& other_vec) const {
-        
+    float dot_product(const Vector3& other_vec) const {
+        return (this->x * other_vec.x) + (this->y * other_vec.y) + (this->z * other_vec.z);
     }
     void normalize() {
-        
+        float length = get_length();
+        x /= length;
+        y /= length;
+        z /= length;
+    }
+
+
+
+    
+    friend std::ostream& operator<<(std::ostream& os, const Vector3& vec) {
+        os << "{ " << vec.x << ", " << vec.y << ", " << vec.z << " }";
+        return os;
     }
 };
 
 int main()
 {
+    Vector3 vec = { 1, 5, 3 };
+    Vector3 vec2 = { 4, 2, 5 };
 
-    std::cout << "Hello World!\n";
+    
+    std::cout << "sum of vectors:" << std::endl;
+    std::cout << vec + vec2 << std::endl;
+    std::cout << "\n" << std::endl;
+
+    std::cout << "subtraction of vectors:" << std::endl;
+    std::cout << vec - vec2 << std::endl;
+    std::cout << "\n" << std::endl;
+
+    std::cout << "length of vec:" << std::endl;
+    std::cout << vec.get_length() << std::endl;
+    std::cout << "\n" << std::endl;
+
+    std::cout << "dot product of vec:" << std::endl;
+    std::cout << vec.dot_product(vec2) << std::endl;
 
     return 0;
 }
